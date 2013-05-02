6231c511fdcf4945060611ed838713a3a4bae87b
Vincent Bostoen <vincent.bostoen@gmail.com>
2013-05-02 23:44:54 +0200

    Remove error handling as it is not used for the moment

--- web/run_my_dart_code.dart
+++ web/run_my_dart_code.dart
@@ -24,27 +24,3 @@ void _writeContentToFile(FileWriter fileWriter){
     Blob blob = new Blob(aFileParts);
     fileWriter.write(blob);
 }
-
-void _handleError(FileError e) {
-  var msg = '';
-  switch (e.code) {
-    case FileError.QUOTA_EXCEEDED_ERR:
-      msg = 'QUOTA_EXCEEDED_ERR';
-      break;
-    case FileError.NOT_FOUND_ERR:
-      msg = 'NOT_FOUND_ERR';
-      break;
-    case FileError.SECURITY_ERR:
-      msg = 'SECURITY_ERR';
-      break;
-    case FileError.INVALID_MODIFICATION_ERR:
-      msg = 'INVALID_MODIFICATION_ERR';
-      break;
-    case FileError.INVALID_STATE_ERR:
-      msg = 'INVALID_STATE_ERR';
-      break;
-    default:
-      msg = 'Unknown Error';
-      break;
-  }
-}
