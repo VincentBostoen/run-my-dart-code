d99c0bf701324501c8dc2b14288d768db85fc223
Vincent Bostoen <vincent.bostoen@gmail.com>
2013-05-06 21:56:34 +0200

    Start running code from page

--- web/run_my_dart_code.dart
+++ web/run_my_dart_code.dart
@@ -4,6 +4,11 @@ import 'dart:collection';
 import 'dart:isolate';
 
 void main() {
+  var runCodeButton = document.getElementById("run-code");
+  runCodeButton.onClick.listen((event) => saveAndRunCode());
+}
+
+void saveAndRunCode(){
   String _codeContentFileURI;
   window.requestFileSystem(1024*1024).then((FileSystem filesystem){
     window.console.log("Access to file system granted");
@@ -12,15 +17,16 @@ void main() {
         _writeContentToFile(fileWriter);
         _codeContentFileURI = fileEntry.toUrl();
         window.console.log("Running content of $_codeContentFileURI");
-        spawnUri(_codeContentFileURI);
+        var sendPort = spawnUri(_codeContentFileURI);
+//        sendPort.call(getPluginMetadata).then((reply) {print(reply);});
       });
     });
-  });
+  });  
 }
 
 void _writeContentToFile(FileWriter fileWriter){
     window.console.log("Writing content to file");
-    var aFileParts = ["void main() {print('hello from an isolate!');}"];
+    var aFileParts = [document.getElementById("code-editor").text];
     Blob blob = new Blob(aFileParts);
     fileWriter.write(blob);
 }
